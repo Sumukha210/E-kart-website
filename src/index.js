60cc18cec6dc4978148bbe0d79104b682ea9938e
Sumukha210 <sumukhakb@gmail.com>
2021-02-04 11:38:02 +0530

    removing redux dev tools for production

--- src/index.js
+++ src/index.js
@@ -8,13 +8,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
 import { RootReducer } from "./Redux/Reducers/index";
 
-const store = createStore(
-  RootReducer,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+const store = createStore(RootReducer, compose(applyMiddleware(thunk)));
+// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 
 ReactDOM.render(
   <>
